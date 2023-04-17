8d3ac32777369314ef54bf2f21ede7f038b3d6b1
Bob <dnruiz87@gmail.com>
2023-04-17 10:22:20 +0200

    Practicas clase

---
+++ lua/practice3_declaracion.lua
@@ -0,0 +1,8 @@
+num = 10515613216815135151
+
+
+if num % 2 == 0 then
+    print("Es par")
+else 
+    print("No es par")
+end
---
+++ lua/testing.lua
@@ -0,0 +1,14 @@
+-- hello.lua
+
+io.write("Buenos dias por la mañana",_VERSION,"!\n")
+
+a="uno"
+b="dos"
+c="tres"
+d="cuatro"
+
+print(a,c)
+
+Tu_nombre = "Bob"
+
+print("Hola",Tu_nombre)
