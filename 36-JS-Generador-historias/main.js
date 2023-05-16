be76ab36bc0324da77defa82ac7bf2dec6fba4a3
Bobdisappear <128700177+Bobdisappear@users.noreply.github.com>
2023-05-16 11:13:39 +0200

    Generador de historias

--- 36-JS-Generador-historias/main.js
+++ 36-JS-Generador-historias/main.js
@@ -10,28 +10,6 @@ function randomValueFromArray(array){
 }
 
 
-// 3. EVENT LISTENER AND PARTIAL FUNCTION DEFINITION
-
-randomize.addEventListener('click', result);
-
-function result() {
-
-  if(customName.value !== '') {
-    const name = customName.value;
-
-  }
-
-  if(document.getElementById("uk").checked) {
-    const weight = Math.round(300);
-    const temperature =  Math.round(94);
-
-  }
-
-  story.textContent = ;
-  story.style.visibility = 'visible';
-}
-
-
 // 2. RAW TEXT STRINGS
 
 const storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.'
@@ -41,3 +19,38 @@ const insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas']
 const insertY = ['the soup kitchen', 'Disneyland','the White House']
 
 const insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away']
+
+// 3. EVENT LISTENER AND PARTIAL FUNCTION DEFINITION
+
+randomize.addEventListener('click', result);
+
+function result() {
+let newStory = storyText;
+let xItem = randomValueFromArray(insertX);  
+let yItem = randomValueFromArray(insertY);
+let zItem = randomValueFromArray(insertZ);
+
+newStory = newStory.replaceAll(':insertx:', xItem);
+newStory = newStory.replaceAll(':inserty:', yItem);
+newStory = newStory.replaceAll(':insertz:', zItem);
+
+
+  if(customName.value !== '' ) {
+    let name = customName.value;
+    newStory = newStory.replaceAll('Bob', name);
+
+  }
+
+  if(document.getElementById("uk").checked) {
+    const weight = Math.round(300/14) + ' Stone';
+    const temperature =  Math.round((94 -32) * 5 / 9) + ' Celsius';
+
+    newStory = newStory.replaceAll('94 farenheit', temperature );
+    newStory = newStory.replaceAll('300 pounds', weight);
+
+
+  }
+
+  story.textContent = newStory;
+  story.style.visibility = 'visible';
+}
