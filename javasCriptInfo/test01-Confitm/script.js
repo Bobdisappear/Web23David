0e27e41e390e4c16446d590ab12db582bc8f0066
Bobdisappear <128700177+Bobdisappear@users.noreply.github.com>
2023-05-16 15:48:03 +0200

    jsinfo

---
+++ javasCriptInfo/test01-Confitm/script.js
@@ -0,0 +1,24 @@
+
+
+// let isBoss = confirm('Are you the boss?');
+// console.log(isBoss);
+// const yes = document.getElementById('yes')
+// const no = document.getElementById('no')
+// if (isBoss){
+//     true = yes
+// }else
+//     no
+
+
+    // La sintaxis:
+
+    result = confirm(question);
+    // La función confirmmuestra una ventana modal con un questiony dos botones: Aceptar y Cancelar.
+    
+    // El resultado es truesi se presiona OK y falseen caso contrario.
+    
+    // Por ejemplo:
+    
+    let isBoss = confirm("Are you the boss?");
+    
+    alert( isBoss ); // true if OK is pressed
