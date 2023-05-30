3934108513b647addea686ff8c1ab6c844d198d6
bobdisappear <dnruiz87@gmail.com>
2023-05-30 20:30:42 +0200

    arraGenerator

--- 35-JS-DOM/script06.js
+++ 35-JS-DOM/script06.js
@@ -51,7 +51,7 @@ add.addEventListener('click', addItem =>{
         const i = document.createElement('i');
         i.className = 'fa-solid fa-xmark'
         const thirdItem = document.querySelector('li:nth-child(3)')
-        ul.insertBefore(li, thirdItem)
+        ul.insertBefore(li,thirdItem)
 
         ul.appendChild(li)
         li.appendChild(btn)
@@ -60,34 +60,43 @@ add.addEventListener('click', addItem =>{
         const newBtnRed = document.querySelectorAll('.remove-item')
 
         newBtnRed.forEach((item)=>{
-        item.addEventListener('click',removeParent =>{
+        item.addEventListener('click',() =>{
         item.parentElement.remove()
 
-    return removeParent
-})  
+    })  
 })
-        
-    return addItem
+
+return addItem
 })  
 }
 addNewItem();
-    
 
+//Recorrer array de li y guardarlos
+
+const ArrayList = []
+const liList = document.querySelectorAll("li")
+liList.forEach((arrayLi) =>{
+    ArrayList.push(arrayLi)
+    return
+})
+
+//auque me genere li dentro de elements estos no los reconoce. solo recorre los predefinidos (temporales?)
 
 //Seleccionar boton y remover lista
 const btn = document.getElementById('clear')
 
-btn.addEventListener('click',removeList =>{
+btn.addEventListener('click',() =>{
 
-    const ul = document.querySelector("ul")
-    const li = document.querySelector("li")
-    ul.remove(li)
-
-    return removeList
-    
+    const li = document.querySelectorAll("li")
+    li.forEach((list) =>{
+        
+        list.remove()
+    })
+    return 
 })  
 
-//Remover un solo boton
+
+//Remover un solo boton recorriendo la lista para no perder 'ul'
 
 const btnRed = document.querySelectorAll('.remove-item')
 
@@ -101,10 +110,3 @@ btnRed.forEach((item)=>{
 })
 
 
-
-
- createNewToRemoveLis()
-
-const div = document.querySelector('filter')
-const ulB = document.createElement('ul')
-div.appendChild(ulB)
